b0d3fb0207e4f73b9e3706f9c5b77f5225828eb6
nyamwaya <alecksonnyamwaya@gmail.com>
2019-01-13 15:14:35 -0600

    business logic implementation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        theme: ThemeData.dark(),
-        home: Scaffold(
-          body: Text(
-            "TRY"
-          )
+    return LoginBlocProvider(
+      child: GoalsBlocProvider(
+        child: MaterialApp(
+          theme: ThemeData(
+            accentColor: Colors.black,
+            primaryColor: Colors.amber,
+          ),
+          home: Scaffold(
+            appBar: AppBar(
+              title: Text(
+                "Goals",
+                style: TextStyle(color: Colors.black),
+              ),
+              backgroundColor: Colors.amber,
+              elevation: 0.0,
+            ),
+            body: LoginScreen(),
+          ),
         ),
-      );
+      ),
+    );
   }
 }
