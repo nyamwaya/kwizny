5b4b417ad02f640d2a68720ba27f4a28cffc8161
nyamwaya <alecksonnyamwaya@gmail.com>
2019-01-15 18:05:25 -0600

    new implementation

---
+++ lib/src/blocs/auth_event_bloc.dart
@@ -0,0 +1,18 @@
+
+abstract class AuthenticationEvent extends BlocEvent {
+  final String name;
+
+  AuthenticationEvent({
+    this.name: '',
+  });
+}
+
+class AuthenticationEventLogin extends AuthenticationEvent {
+  AuthenticationEventLogin({
+    String name,
+  }) : super(
+    name: name,
+  );
+}
+
+class AuthenticationEventLogout extends AuthenticationEvent {}
