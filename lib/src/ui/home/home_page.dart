fc5adb4c280f74fce426f9c5f4ed99ebdc7bad16
nyamwaya <alecksonnyamwaya@gmail.com>
2019-02-24 17:32:42 -0600

    fix back to login from home

--- lib/src/ui/home/home_page.dart
+++ lib/src/ui/home/home_page.dart
@@ -1,9 +1,20 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:kwizny/src/ui/home/home_screen.dart';
 
 class HomePage extends StatelessWidget {
+
+  Future<bool> _onWillPopScope() async {
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return HomeScreen();
+   // return HomeScreen();
+   return WillPopScope(
+     onWillPop: _onWillPopScope,
+     child: HomeScreen(),
+   );
   }
 }
